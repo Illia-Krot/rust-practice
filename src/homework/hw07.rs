d48b77eca2bb10ece43528525ac3be8340419af4
Illia-Krot <130045919+Illia-Krot@users.noreply.github.com>
2025-08-22 12:22:53 +0300

    Update hw07.rs

--- src/homework/hw07.rs
+++ src/homework/hw07.rs
@@ -1,19 +1,13 @@
-pub fn is_prime(n: u64) -> bool {
-    if n <= 1 {
-        return false;
-    }
-    if n <= 3 {
-        return true;
-    }
-    if n % 2 == 0 || n % 3 == 0 {
-        return false;
-    }
-    let mut i = 5;
-    while i * i <= n {
-        if n % i == 0 || n % (i + 2) == 0 {
-            return false;
-        }
-        i += 6;
-    }
-    true
+pub fn swap_case(s: &str) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().to_string()
+            } else if c.is_uppercase() {
+                c.to_lowercase().to_string()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
 }
